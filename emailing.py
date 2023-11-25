4daccdee4bda55ed9070222e10a66a875da922ff
CodeSolaris <aleksandr.volkov.it@gmail.com>
2023-11-25 14:35:35 +0200

    * feat(emailing.py): add function to send an email with an attached image * feat(main.py): add functionality to capture frames, detect objects, and send email with attached image

---
+++ emailing.py
@@ -0,0 +1,44 @@
+import imghdr
+import os
+import smtplib
+from email.message import EmailMessage
+
+from dotenv import load_dotenv
+
+load_dotenv()
+PASSWORD = os.getenv("API_KEY")
+SENDER = "a.volkov2509@gmail.com"
+RECEIVER = "a.volkov2509@gmail.com"
+def send_email(image_path):
+    """
+    Sends an email with an attached image.
+
+    Args:
+        image_path (str): The path to the image file.
+
+    Raises:
+        Exception: If an error occurs while sending the email.
+
+    """
+    try:
+        email_message = EmailMessage()
+        email_message["Subject"] = "new object detected"
+        email_message["From"] = SENDER
+        email_message["To"] = RECEIVER
+        email_message.set_content("Hey, we just saw a new customer!")
+
+        with open(image_path, "rb") as image_file:
+            content = image_file.read()
+
+        email_message.add_attachment(content, maintype="image", subtype=imghdr.what("", content))
+
+        with smtplib.SMTP("smtp.gmail.com", 587) as gmail:
+            gmail.ehlo()
+            gmail.starttls()
+            gmail.login(SENDER, PASSWORD)
+            gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    except Exception as e:
+        print("An error occurred while sending the email:", str(e))
+
+if __name__ == "__main__":
+    send_email(image_path="images/1.jpg")
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import cv2
-
+from emailing import send_email
+from glob import glob
+import os
+from threading import Thread
+from time import sleep
 # Initialize video capture
 video_capture = cv2.VideoCapture(0)
 
 # Set initial frame as None
 first_frame = None
+status_list = []
+count = 1
 
+def clean_folder():
+    all_images = glob("images/*.jpg")
+    for image in all_images:
+        os.remove(image)
 while True:
+    status = 0
     # Read frame from video capture
     check, frame = video_capture.read()
     if not check:
@@ -30,24 +41,48 @@ while True:
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
     # Display the resulting frame
-    cv2.imshow("Capturing", dil_frame)
+    # cv2.imshow("Capturing", dil_frame)
 
     # Find contours in the dilated frame
-    contours, _ = cv2.findContours(
+    contours, check = cv2.findContours(
         dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
     )
 
     # Iterate over contours and draw rectangles around large enough contours
     for contour in contours:
-        if cv2.contourArea(contour) < 10000:
+        if cv2.contourArea(contour) < 7000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        if rectangle.any():
+            status = 1
+            cv2.imwrite(f"images/{count}.jpg", frame)
+            count += 1
+            all_images = glob("images/*.jpg")
+            index = int(len(all_images) / 2)
+            image_path = all_images[index]
 
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        thread_email = Thread(target=send_email, args=(image_path,))
+        thread_email.daemon = True
+        thread_clean = Thread(target=clean_folder)
+        thread_clean.daemon = True
+
+        thread_email.start()
+        
+
+    cv2.imshow("Video", frame)
     # Check for 'q' key press to exit the loop
     key = cv2.waitKey(1)
     if key == ord("q"):
+        thread_clean.start()
+        sleep(5)
         break
 
 # Release the video capture
 video_capture.release()
+
+
